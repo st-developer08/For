c28eecdda913bfcd75a29382ea819ad27a96d4f3
st-developer08 <st.developer.0830@gmail.com>
2025-07-03 12:55:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+for(;;){
+    let number = +prompt('Введите число больше 100:')
+    if(number >= 100){
+        alert('гуд')
+        break;
+    }
+   
+}
+
+
+
+
+let arr = [4,16,19,22,11,144,967,19124]
+console.log(arr);
+
+let even = []
+let odd = []
+
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0){
+        even.push(arr[i])
+    }else{
+        odd.push(arr[i])
+    }
+   
+}
+console.log(`Четные числа;${even}`);
+console.log(`Нечетные числа;${odd}`);
+           
